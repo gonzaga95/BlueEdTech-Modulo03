d483f868e54a6d99aefcceaa7c94f5ded033a816
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-05 21:11:47 -0400

    update no index

--- index.js
+++ index.js
@@ -3,40 +3,48 @@ const cors = require('cors');
 
 const app = express();
 
+const controller = require('./controllers/animes.controller');
+
 app.use(cors());
 
-let animes = require('./mocks/animes');
+// let animes = require('./mocks/animes');
 
-const Char = require('./entities/character.entity');
+// const Char = require('./entities/character.entity');
 
-app.get('/', function (req, res) {
-    const char1 = new Char({
-        name: 'dudu',
-        lastName: 'testson',
-        skill: 'programar',
-        age: '30',
-        gender: 'male',
-    });
-    char1.validate();
+app.get('/', controller.findAllAnimes);
+app.get('/anime/:id', controller.findAnimeById);
+app.get('/anime', controller.createAnime);
+app.get('/anime', controller.updateAnime);
+app.get('/anime/:id', controller.deleteAnime);
 
-    res.send(animes);
-});
+// app.get('/', function (req, res) {
+//     const char1 = new Char({
+//         name: 'dudu',
+//         lastName: 'testson',
+//         skill: 'programar',
+//         age: '30',
+//         gender: 'male',
+//     });
+//     char1.validate();
 
-app.get('/anime/:id', function (req, res) {
-    const animeId = req.params.id;
-    let animeFinded;
+//     res.send(animes);
+// });
 
-    animes.map((anime) => {
-        if (anime.id === animeId) {
-            animeFinded = anime;
-        }
-    });
-    if (animeFinded) {
-        res.send(animeFinded);
-    } else {
-        res.send('Nenhum anime com esse ID foi encontrado');
-    }
-});
+// app.get('/anime/:id', function (req, res) {
+//     const animeId = req.params.id;
+//     let animeFinded;
+
+//     animes.map((anime) => {
+//         if (anime.id === animeId) {
+//             animeFinded = anime;
+//         }
+//     });
+//     if (animeFinded) {
+//         res.send(animeFinded);
+//     } else {
+//         res.send('Nenhum anime com esse ID foi encontrado');
+//     }
+// });
 
 app.listen(3000, () => {
     console.log(`A aplicação está rodando na porta 3000`);
